391d72bfd4ccb8bece9654fb0a66070d12b776e3
HsiangHo <ho_cheung@qq.com>
2017-12-09 22:24:19 +0800

    added workspace

---
+++ mas-plugin-loader/mas-plugin-loader/mas-plugin-loader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mas-plugin-loader
+//
+//  Created by Jovi on 12/9/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ mas-plugin-loader/mas-plugin-loader/mas-plugin-loader/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  mas-plugin-loader
+//
+//  Created by Jovi on 12/9/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
