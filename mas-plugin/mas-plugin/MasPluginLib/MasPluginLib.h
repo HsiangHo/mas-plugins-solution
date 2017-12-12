b968103a3fca166e39997690996759aae5a9fc9a
HsiangHo <ho_cheung@qq.com>
2017-12-12 22:52:32 +0800

    added MasPluginLib

---
+++ mas-plugin/mas-plugin/MasPluginLib/MasPluginLib.h
@@ -0,0 +1,20 @@
+//
+//  MasPluginLib.h
+//  MasPluginLib
+//
+//  Created by Jovi on 12/12/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MasPluginLib.
+FOUNDATION_EXPORT double MasPluginLibVersionNumber;
+
+//! Project version string for MasPluginLib.
+FOUNDATION_EXPORT const unsigned char MasPluginLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MasPluginLib/PublicHeader.h>
+
+#import <MasPluginLib/MPPluginObject.h>
+#import <MasPluginLib/MPPackageWrapper.h>
