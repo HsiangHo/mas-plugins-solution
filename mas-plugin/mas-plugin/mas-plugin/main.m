bd889c2a8780238ae08d18d1b36400cb7a33ec91
HsiangHo <ho_cheung@qq.com>
2017-12-12 23:06:02 +0800

    added demo

--- mas-plugin/mas-plugin/mas-plugin/main.m
+++ mas-plugin/mas-plugin/mas-plugin/main.m
@@ -7,10 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MasPluginLib/MasPluginLib.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        [MPPackageWrapper packPlugin:@"/Users/jovi/Desktop/untitled folder 2" key:@"123456" outPath:@"/Users/jovi/Desktop/1.xy"];
+        MPPluginObject *plugin = [[MPPluginObject alloc] initWithMPPluginFile:@"/Users/jovi/Desktop/1.xy" withKey:@"123456"];
+        [plugin loadPlugin];
+        [plugin setLoaderPath:@"/untitled folder 2/mas-plugin-loader.app"];
+        [plugin launchExec:@"/untitled folder 2/plugin.app/Contents/MacOS/plugin" withArgument:@[@"1"] withPrivilege:YES];
+        sleep(10);
+        [plugin unloadPlugin];
+        
         NSLog(@"Hello, World!");
     }
     return 0;
